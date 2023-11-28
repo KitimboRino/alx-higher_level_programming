ff8763707e2d5068b24ee65482ebbe83f3101d6e
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-11-28 01:32:09 -0800

    fix: 8. To uppercase taska nd run tests

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 def uppercase(input_str):
+    result = ""  # Initialize an empty string to store the result
     for char in input_str:
-        # Check if the character is a lowercase letter
         if ord('a') <= ord(char) <= ord('z'):
-            # Convert to uppercase by subtracting the ASCII difference
-            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
-            print(uppercase_char, end="")
+            # Convert to uppercase by subtracting the ASCII difference and append to result
+            result += chr(ord(char) - ord('a') + ord('A'))
         else:
-            # Print the character as is
-            print(char, end="")
+            # Append the character as is to result
+            result += char
     
-    # Print a new line after processing the entire string
-    print()
+    # Use a single print function to print the entire result followed by a new line
+    print(result)
