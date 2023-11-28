9df52565065bd358f41644676f0e5b65c17c987f
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-11-28 05:04:38 -0800

    fix: 6. Inventing is a combination of brains and materials.  task

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
+# Iterate over the tens place (i) from 0 to 8
 for i in range(9):
+    # Iterate over the ones place (j) from i+1 to 9
     for j in range(i + 1, 10):
-        if i * 10 + j <= 89:
-            print("{:02d}".format(i * 10 + j), end=", ")
-print("{:02d}".format(89))
+        # Check if the concatenated number (i*10 + j) is less than 89
+        if i * 10 + j < 89:
+            # Print the concatenated number with the specified format
+            print("{:d}{:d}".format(i, j), end=", ")
 
+# Print the number 89 separately after the loop
+print("{:d}".format(89))
