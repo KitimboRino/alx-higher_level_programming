cfe24056a0094e64419076bf11ecaf5877fcb112
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-11-28 01:07:18 -0800

    feat: 7. islower task and run tests

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i * 10 + j <= 89:
+            print("{:02d}".format(i * 10 + j), end=", ")
+print("{:02d}".format(89))
+
