0343f30589f8b977436c59be556c8decb2b7dde3
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-11-28 03:39:24 -0800

    feat: 9. There are only 3 colors task and run tests

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    # Handle negative numbers by using abs() and take the remainder when dividing by 10
+    # Check if the number is negative
     if number < 0:
-        last_num = abs(number) % 10
-    else:
-        # For non-negative numbers, simply take the remainder when dividing by 10
+        # If negative, calculate the last digit using the modulo operator
+        # The negative sign is applied to ensure the result is negative
+        last_num = (-number % 10)
+    # If the number is zero or positive
+    elif number >= 0:
+        # Calculate the last digit using the modulo operator
         last_num = number % 10
-    
-    # Print the last digit followed by a new line
+
+    # Print the last digit using formatted string and prevent a newline with end=""
     print("{:d}".format(last_num), end="")
 
-    # Return the value of the last digit
-    return last_num
+    # Return the last digit
+    return (last_num)
