08cb5e58b419f51ccd9c5e8f6370a6e700a47097
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-11-28 19:39:58 -0800

    fix: 8. To uppercase task and run tests

---
+++ 8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(input_str):
+    for char in input_str:
+        if ord('a') <= ord(char) <= ord('z'): 
+            char = chr(ord(char) - ord('a') + ord('A'))
+        print("{:s}".format(char), end="")
+    print()
