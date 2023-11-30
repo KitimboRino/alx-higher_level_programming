83c1bb51053c2381eb976debe65d2a792827474e
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-11-30 10:19:13 -0800

    fix: 6. Build my own calculator! task and run tests

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,6 +2,7 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
+
 def my_calculator():
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -21,9 +22,9 @@ def my_calculator():
         result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
+    sys.exit(1)
     print(f"{a} {operator} {b} = {result}")
+
 
 if __name__ == "__main__":
     my_calculator()
