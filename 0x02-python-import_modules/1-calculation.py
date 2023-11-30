fc2131cf8e8cc9cbb9d7f2dcb05424cf653c24a0
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-11-30 05:08:51 -0800

    fix: 1. My first toolbox! task

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,22 +2,16 @@
 
 if __name__ == "__main__":
     # Check if the script is being run directly
-
     import sys
     # Import the sys module to access command-line arguments
-
     from calculator_1 import add, sub, mul, div
     # Import specific functions from the calculator_1 module
-
     argv = sys.argv[1:]
     # Get command-line arguments, excluding the script name
-
     argv_count = len(argv)
     # Count the number of command-line arguments
-
     operators = ["+", "-", "*", "/"]
     # Define a list of valid operators
-
     if argv_count != 3:
         # Check if the number of command-line arguments is not equal to 3
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -30,11 +24,9 @@ if __name__ == "__main__":
         exit(1)
     else:
         # If conditions are met, proceed with calculations
-
         a = int(sys.argv[1])
         b = int(sys.argv[3])
         # Convert command-line arguments to integers
-
         if sys.argv[2] == "+":
             # Check the operator and perform addition
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
