6e43ca2bbb4dc0edb758703923d5ff8bcf4cf313
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2023-12-07 02:32:40 -0800

    fix: 15. Delete by value task

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,5 @@
+#!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    # Iterate the dic items and delete keys with the specified value
     for key in list(a_dictionary):
         if a_dictionary[key] == value:
             del a_dictionary[key]
