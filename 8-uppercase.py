06dc7dc0ab619fe94c3853e8f0b9a8b7d5d1b7fd
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2024-01-02 00:45:50 -0800

    feat: created directory TDD

--- 8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def uppercase(input_str):
-    for char in input_str:
-        if ord('a') <= ord(char) <= ord('z'): 
-            char = chr(ord(char) - ord('a') + ord('A'))
-        print("{:s}".format(char), end="")
-    print()
