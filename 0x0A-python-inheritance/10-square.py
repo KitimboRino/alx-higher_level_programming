5a1fe6e22af5a2c3f19257afdf1a00071aa8aa23
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2024-01-08 11:22:18 -0800

    fix: 10. Square #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-'''
-more class base
-'''
+"""
+More class base
+"""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
-
-'''
+"""
 Square class
-'''
+"""
 
 
 class Square(Rectangle):
-    ''' Square Class '''
+    """ Square Class """
     def __init__(self, size):
-        ''' instantiation with size '''
+        """ Instantiation with size """
         self.__size = size
         super().__init__(self.__size, self.__size)
