11828ba5a918d9ee61b20cb788a6311bacac67d1
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2024-01-08 10:35:58 -0800

    task: 4. Only sub class of and run tests

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' module: 4-inherits_from
+'''
+def inherits_from(obj, a_class):
+    '''the object is an instance of a class that inherited (directly or indirectly)
+    obj: an object
+    a_class: a class
+    returns None
+    '''
+    return False if type(obj) is a_class else isinstance(obj, a_class)
