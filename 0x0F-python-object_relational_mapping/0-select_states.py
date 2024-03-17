acee51a967cfc3d13c318e93854d8a5c68dc820f
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2024-03-17 00:29:00 -0700

    fix: 0. Get all states and run tests

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,12 +7,12 @@ import MySQLdb
 # Main condition check
 if __name__ == "__main__":
     db = MySQLdb.connect(
-            host = 'localhost',
-            port = 3306,
-            user = argv[1],
-            passwd = argv[2],
-            db = argv[3],
-            charset = 'utf8')
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset='utf8')
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` ORDDER BY `id` ASC")
     [print(state) for state in cur.fetchall()]
