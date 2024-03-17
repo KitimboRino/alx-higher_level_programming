17375af3ceda5c4572335486e552cd55eb2ebfa7
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2024-03-17 02:51:25 -0700

    task: 1. Filter states and run tests

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 """
 Lists all states with a name satsting with N from
-the database hbtn_0e_0_usa.
+the database hbtn_0e_0_usa
 """
-
-from sys import argv
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    db = MysSQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
-            charset='utf8')
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in cur.fetchall() if state[1][0] == "N"]
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
